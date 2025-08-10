2c26c99dd11a73ba594580c76d3e67c6cee58e7d
ThaiThuan <thaithuan.work@gmail.com>
2025-08-10 16:16:19 +0700

    final-2

--- services/gateway/src/main/java/org/ecommerce/security/SecurityConfig.java
+++ services/gateway/src/main/java/org/ecommerce/security/SecurityConfig.java
@@ -1,4 +1,29 @@
-package org.ecommerce.sercurity;
+package org.ecommerce.security;
 
-public class SercurityConfig {
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+    @Value("${keycloak.client-id}")
+    private String kcClientId;
+
+    @Bean
+    public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeExchange(exchange -> exchange
+                        .pathMatchers("/eureka/**").permitAll()
+                        // customer
+                        .anyExchange().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+        return http.build();
+    }
 }
